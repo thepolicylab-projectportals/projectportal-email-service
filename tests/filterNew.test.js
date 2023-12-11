0ebf3629e9b852499248e5343743bdc2ca12967b
Josh <joshua_lu1@brown.edu>
2023-12-11 12:15:15 -0500

    update jest testing

--- tests/filterNew.test.js
+++ tests/filterNew.test.js
@@ -4,26 +4,26 @@ import {filterNew} from "../src/lib";
 describe('filterNew()', () => {
 
   const todayDate = new Date()
-  const date1 = subMonths(todayDate, 3).toISOString()
-  const date2 = subMonths(todayDate, 1).toISOString()
-  const date3 = subWeeks(todayDate, 2).toISOString()
-  const date4 = subDays(todayDate, 2).toISOString()
-  const date5 = subDays(todayDate, 1).toISOString()
+  const threeMonthsAgo = subMonths(todayDate, 3).toISOString()
+  const oneMonthAgo = subMonths(todayDate, 1).toISOString()
+  const twoWeeksAgo = subWeeks(todayDate, 2).toISOString()
+  const twoDaysAgo = subDays(todayDate, 2).toISOString()
+  const oneDayAgo = subDays(todayDate, 1).toISOString()
 
 
 
   it('returns filtered projects that are new based on subDays', () => {
     const projects = [
-      { created: date3 },
-      { created: date4 },
-      { created: date5 },
+      { created: twoWeeksAgo },
+      { created: twoDaysAgo },
+      { created: oneDayAgo },
     ]
 
     process.env.subDays = '3'
     const filteredProjects = filterNew(projects, todayDate)
     const expectedProjects = [
-      { created: date4 },
-      { created: date5 },
+      { created: twoDaysAgo },
+      { created: oneDayAgo },
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -31,9 +31,9 @@ describe('filterNew()', () => {
 
   it('returns no projects when none are new based on subDays', () => {
     const projects = [
-      { created: date3 },
-      { created: date4 },
-      { created: date5 },
+      { created: twoWeeksAgo },
+      { created: twoDaysAgo },
+      { created: oneDayAgo },
     ]
 
     process.env.subDays = '1'
@@ -45,16 +45,16 @@ describe('filterNew()', () => {
 
   it('returns filtered projects that are new based on subMonths', () => {
     const projects = [
-      { created: date1 },
-      { created: date2 },
-      { created: date3 },
+      { created: threeMonthsAgo },
+      { created: oneMonthAgo },
+      { created: twoWeeksAgo },
     ]
 
     process.env.subMonths = '2'
     const filteredProjects = filterNew(projects, todayDate)
     const expectedProjects = [
-      { created: date2 },
-      { created: date3 },
+      { created: oneMonthAgo },
+      { created: twoWeeksAgo },
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
--- tests/filterStale.test.js
+++ tests/filterStale.test.js
@@ -5,24 +5,24 @@ describe('filterStale()', () => {
 
   const todayDate = new Date(2023, 8, 8)
 
-  const dateA = new Date(2023, 8, 4)
-  const dateB = new Date(2023, 8, 10)
+  const dateBeforeTodayDate = new Date(2023, 8, 4)
+  const datePastTodayDate = new Date(2023, 8, 10)
 
-  const dateC = subDays(todayDate, 91)
-  const dateD = subDays(todayDate, 5)
-  const dateE = subDays(todayDate, 90)
+  const ninetyOneDaysBeforeTodayDate = subDays(todayDate, 91)
+  const fiveDaysBeforeTodayDate = subDays(todayDate, 5)
+  const ninetyDaysBeforeTodayDate = subDays(todayDate, 90)
 
 
   it('returns identified stale open projects based on closeDate field', () => {
     const projects = [
-      {opportunityCloses: dateA, status: "open"},
-      {opportunityCloses: dateB, status: "open"},
+      {opportunityCloses: dateBeforeTodayDate, status: "open"},
+      {opportunityCloses: datePastTodayDate, status: "open"},
       {opportunityCloses: todayDate, status: "open"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { opportunityCloses: dateA, status: "open", problems: ["The opportunity closes date has passed, but project is marked open."] }
+      { opportunityCloses: dateBeforeTodayDate, status: "open", problems: ["The opportunity closes date has passed, but project is marked open."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -30,7 +30,7 @@ describe('filterStale()', () => {
 
   it('returns no identified stale open projects based on closeDate field', () => {
     const projects = [
-      {opportunityCloses: dateB, status: "open"},
+      {opportunityCloses: datePastTodayDate, status: "open"},
       {opportunityCloses: todayDate, status: "open"}
     ]
 
@@ -42,14 +42,14 @@ describe('filterStale()', () => {
 
   it('returns identified stale open projects based on startDate field', () => {
     const projects = [
-      {startDate: dateA, status: "open"},
-      {startDate: dateB, status: "open"},
+      {startDate: dateBeforeTodayDate, status: "open"},
+      {startDate: datePastTodayDate, status: "open"},
       {startDate: todayDate, status: "open"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { startDate: dateA, status: "open", problems: ["The project start date has passed, but project is marked open."] }
+      { startDate: dateBeforeTodayDate, status: "open", problems: ["The project start date has passed, but project is marked open."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -57,7 +57,7 @@ describe('filterStale()', () => {
 
   it('returns no identified stale open projects based on startDate field', () => {
     const projects = [
-      {startDate: dateB, status: "open"},
+      {startDate: datePastTodayDate, status: "open"},
       {startDate: todayDate, status: "open"}
     ]
 
@@ -69,14 +69,14 @@ describe('filterStale()', () => {
 
   it('returns identified stale open projects based on lastModified field', () => {
     const projects = [
-      {lastModified: dateC, status: "open"},
-      {lastModified: dateD, status: "open"},
-      {lastModified: dateE, status: "open"}
+      {lastModified: ninetyOneDaysBeforeTodayDate, status: "open"},
+      {lastModified: fiveDaysBeforeTodayDate, status: "open"},
+      {lastModified: ninetyDaysBeforeTodayDate, status: "open"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { lastModified: dateC, status: "open", problems: ["It has been over 90 days since project's last modification."] }
+      { lastModified: ninetyOneDaysBeforeTodayDate, status: "open", problems: ["It has been over 90 days since project's last modification."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -84,8 +84,8 @@ describe('filterStale()', () => {
 
   it('returns no identified stale open projects based on lastModified field', () => {
     const projects = [
-      {lastModified: dateD, status: "open"},
-      {lastModified: dateE, status: "open"}
+      {lastModified: fiveDaysBeforeTodayDate, status: "open"},
+      {lastModified: ninetyDaysBeforeTodayDate, status: "open"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
@@ -96,14 +96,14 @@ describe('filterStale()', () => {
 
   it('returns identified stale open projects based on endDate field', () => {
     const projects = [
-      {endDate: dateA, status: "open"},
-      {endDate: dateB, status: "open"},
+      {endDate: dateBeforeTodayDate, status: "open"},
+      {endDate: datePastTodayDate, status: "open"},
       {endDate: todayDate, status: "open"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { endDate: dateA, status: "open", problems: ["The project end date has passed, but project is marked open."] }
+      { endDate: dateBeforeTodayDate, status: "open", problems: ["The project end date has passed, but project is marked open."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -111,7 +111,7 @@ describe('filterStale()', () => {
 
   it('returns no identified stale open projects based on endDate field', () => {
     const projects = [
-      {endDate: dateB, status: "open"},
+      {endDate: datePastTodayDate, status: "open"},
       {endDate: todayDate, status: "open"}
     ]
 
@@ -123,14 +123,14 @@ describe('filterStale()', () => {
 
   it('returns identified stale ongoing projects based on lastModified field', () => {
     const projects = [
-      {lastModified: dateC, status: "ongoing"},
-      {lastModified: dateD, status: "ongoing"},
-      {lastModified: dateE, status: "ongoing"}
+      {lastModified: ninetyOneDaysBeforeTodayDate, status: "ongoing"},
+      {lastModified: fiveDaysBeforeTodayDate, status: "ongoing"},
+      {lastModified: ninetyDaysBeforeTodayDate, status: "ongoing"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { lastModified: dateC, status: "ongoing", problems: ["It has been over 90 days since project's last modification."] }
+      { lastModified: ninetyOneDaysBeforeTodayDate, status: "ongoing", problems: ["It has been over 90 days since project's last modification."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -138,8 +138,8 @@ describe('filterStale()', () => {
 
   it('returns no identified stale ongoing projects based on lastModified field', () => {
     const projects = [
-      {lastModified: dateD, status: "ongoing"},
-      {lastModified: dateE, status: "ongoing"}
+      {lastModified: fiveDaysBeforeTodayDate, status: "ongoing"},
+      {lastModified: ninetyDaysBeforeTodayDate, status: "ongoing"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
@@ -150,14 +150,14 @@ describe('filterStale()', () => {
 
   it('returns identified stale ongoing projects based on endDate field', () => {
     const projects = [
-      {endDate: dateA, status: "ongoing"},
-      {endDate: dateB, status: "ongoing"},
+      {endDate: dateBeforeTodayDate, status: "ongoing"},
+      {endDate: datePastTodayDate, status: "ongoing"},
       {endDate: todayDate, status: "ongoing"}
     ]
 
     const filteredProjects = filterStale(projects, todayDate)
     const expectedProjects = [
-      { endDate: dateA, status: "ongoing", problems: ["The project end date has passed, but project is marked open."] }
+      { endDate: dateBeforeTodayDate, status: "ongoing", problems: ["The project end date has passed, but project is marked open."] }
     ]
 
     expect(filteredProjects).toEqual(expectedProjects)
@@ -165,7 +165,7 @@ describe('filterStale()', () => {
 
   it('returns no identified stale ongoing projects based on endDate field', () => {
     const projects = [
-      {endDate: dateB, status: "ongoing"},
+      {endDate: datePastTodayDate, status: "ongoing"},
       {endDate: todayDate, status: "ongoing"}
     ]
 
